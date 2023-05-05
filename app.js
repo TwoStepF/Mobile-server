53baffb6047c337414163e6b9518640bcd33e17d
khanh <at160231@actvn.edu.vn>
2023-05-05 18:18:22 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import Koa from "koa";
+import Router from "koa-router";
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello, Koa!";
-});
+router.get("/test", (ctx) => {
+    ctx.body = {
+        name: "hello"
+    }
+})
 
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
